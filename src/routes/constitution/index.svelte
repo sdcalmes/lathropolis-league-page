574033dc00dd3504dcbc7585e6f831fa8493c5f2
sdcalmes <scalmes@wisc.edu>
2022-06-28 23:59:10 -0500

    Updating constitution

--- src/routes/constitution/index.svelte
+++ src/routes/constitution/index.svelte
@@ -8,7 +8,7 @@
     let two, twoOne, twoTwo, twoThree;
     let three, threeOne;
     let four, fourOne, fourTwo;
-    let five, fiveOne;
+    let five, fiveOne, fiveTwo, fiveThree;
     let six, sixOne, sixTwo, sixThree;
     let seven, sevenOne, sevenTwo, sevenThree;
 
@@ -100,7 +100,7 @@
 </style>
 
 <div class="constitution">
-    <h1 class="noUnderscore">LEGENDS LEAGUE CONSTITUTION</h1>
+    <h1 class="noUnderscore">LATHROPOLIS LEAGUE CONSTITUTION</h1>
     
     <h2 class="noUnderscore">TABLE OF CONTENTS</h2>
     
@@ -153,7 +153,7 @@
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Positional Breakdown</h3>
     
-    <p>28 Total Players</p>
+    <p>14 Total Players</p>
     
     <p class="underscore">Starters</p>
     <ul>
@@ -164,14 +164,14 @@
         <li>WR</li>
         <li>TE</li>
         <li>FLEX (RB/WR/TE)</li>
-        <li>FLEX (RB/WR)</li>
         <li>D/ST</li>
         <li>K</li>
     </ul>
     
-    <p>18 Bench (Expanded by 5 between the keague draft and start of regular season)</p>
+    <p>5 Bench</p>
 
-    <p>1 IR Spots (Player must be labeled as IR, Out, or Covid in the Sleeper App)</p>
+    <p>1 IR Spot (Player must be labeled as IR, Out, or Covid in the Sleeper App)</p>
+    <p><strong>Addendum:</strong> For the 2020/21 seasons, we have 10 IR spots. 9 of which are specifically dedicated for COVID marked players.</p>
     
         <h4 bind:this={oneOneOne}>1.1.1 Position Maximums</h4>
         
@@ -189,11 +189,12 @@
 
     <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Trading</h3>
     
-    <p>Trading of players and draft picks (up to 3 drafts away) are allowed. Trades will process immediately and will be reversed if vetoed or under investigation for collusion. Tradable assets include players, draft picks, and FAAB $.</p>
+    <p>Trading of players and FAAB is allowed. Trades will process immediately. Only if there is suspected collusion will the trade enter a review phase. Tradable assets include players and FAAB $.</p>
+    <p><strong>Note:</strong> The trading of 'future promises' is not allowed and will be considered collusion. An example of this is 'promising to trade a player back after a certain period of time'.</p>
     
     <h4 bind:this={oneTwoOne}>1.2.1 Trade Collusion</h4>
     <div class="subBlock">
-        <p>If any owners are suspected of accepting bribes/offering bribes to enhance their/another owners team via trade, trading any assets other than fantasy players, draft picks, FAAB $, or otherwise found guilty of engaging in any form of collusion*, all parties involved in the trade will be susceptible to punishment. The punishments will go as followed:</p>
+        <p>If any owners are suspected of accepting bribes/offering bribes to enhance their/another owners team via trade, trading any assets other than fantasy players or FAAB $, or otherwise found guilty of engaging in any form of collusion*, all parties involved in the trade will be susceptible to punishment. The punishments will go as followed:</p>
         
         <ul>
             <li>First Offense – Immediate reduction of FAAB budget by 80%</li>
@@ -210,17 +211,17 @@
 
     <h4 bind:this={oneTwoTwo}>1.2.2 Lending Players Prohibition</h4>
     <div class="subBlock">
-        <p>Any player traded from a team may not be re-acquired via trade within 6 weeks of the original trade. Any teams involved in this behavior will be subject to the aforementioned collusion punishments.</p>
+        <p>Any player traded from a team may not be re-acquired via trade within 3 weeks of the original trade. Any teams involved in this behavior will be subject to the aforementioned collusion punishments.</p>
     </div>
     
     <h4 bind:this={oneTwoThree}>1.2.3 Trade Deadline</h4>
     <div class="subBlock">
-        <p>The trade deadline will be set for the Saturday of the week 11 games of the NFL season.</p>
+        <p>The trade deadline will be set for the end of the final game of week 14 of the current NFL season.</p>
     </div>
     
     <h4 bind:this={oneTwoFour}>1.2.4 Trade Restrictions</h4>
     <div class="subBlock">
-        <p>You will not be able to trade draft picks that are more than 3 drafts away. For example, during the 2021 season you cannot trade any picks in or after the 2025 draft. You may only trade FAAB $ from the current season.</p>
+        <p></p>
     </div>
     
     <h4 bind:this={oneTwoFive}>1.2.5 Veto/Trade Process</h4>
@@ -232,22 +233,19 @@
     
     <h3 bind:this={oneThree}>1.3 Waiver Wire</h3>
     
-    <p>FAAB Waiver wire system. Each team will begin the season with $100. $0 bids are allowed. Bids will process Thursdays and Sundays at 9AM EST.</p>
+    <p>FAAB Waiver wire system. Each team will begin the season with $100. $0 bids are allowed. Waivers are on a rolling schedule, processing at 2AM CDT Wednesday through Saturday.</p>
 
-    <p>FAAB budgets will reset each season in January.</p>
+    <p>Players must spend 2 days on waivers after being dropped.</p>
+
+    <p>Players that have not been claimed or have passed the 2 day minimum will become free agents on Sunday and Monday.</p>
+
     
     
     <h2 class="sectionHeading" bind:this={two}>Section 2 Drafting</h2>
     
     <h3 bind:this={twoOne}>2.1 Draft Order</h3>
-    <p>The draft order each year will be a linear draft in reverse order of the previous year’s regular season standings.</p>
+    <p>The draft order each year will be determined in random fashion by <a href="https://100yardrush.com/">100 Yard Rush</a> at Dude's Weekend.</p>
 
-    <h3 bind:this={twoTwo}>2.2 Rookie Drafts</h3>
-    <p>Rokkie drafts will consist of 3 round of drafting, with 5 minutes alotted for each pick. To make room for the incoming players, benches will be expanded by (5) spots. These spots will be removed in mid-August, at which point you will need to have cut the necessary players.</p>
-    <p>Any spots not filled during the draft, can be filled immediately afterwards from the waiver wire.</p>
-    
-    <h3 bind:this={twoThree}>2.3 Draft Day Trades</h3>
-    <p>If a trade is being made during the draft, one or both parties can alert the commissioner to pause the draft to allow for extra time to negotiate the trade.</p>
     
     <h2 class="sectionHeading" bind:this={three}>Section 3 Scoring System</h2>
 
@@ -257,7 +255,8 @@
     <h2 class="sectionHeading" bind:this={four}>Section 4 Postseason</h2>
     
     <h3 bind:this={fourOne}>4.1 Playoffs</h3>
-    <p>At the end of the season 6 teams will make the playoffs. The first round of playoffs will commence in week 15. Each team that wins their division and the 4 wild card teams that finish with the best records, will receive a playoff berth. The 2 teams that win their division will receive a bye week in the first round of the playoffs. Each playoff matchup will only last one week and the winner will advance. Week 18 will not be used.</p>
+    <p>At the end of the season 6 teams will make the playoffs. The first round of playoffs will commence in week 15. The top 5 seeded teams will make the playoff with the 6th team being the "at-large" bid. The "at-large" is chosen based on the most Points For out of the remaining teams.
+        The top two seeds will receive a round 1 bye.  Each playoff matchup will only last one week and the winner will advance. Week 18 will not be used.</p>
     
     <h3 bind:this={fourTwo}>4.2 Seeding Tiebreakers</h3>
     <p>If two teams finish with the same record the tiebreakers will go as followed:</p>
@@ -271,37 +270,37 @@
         <li>Duel to the death</li>
     </ol>
 
-    <h2 class="sectionHeading" bind:this={five}>Section 5 Tanking Policy</h2>
-    
-    <h3 bind:this={fiveOne}>5.1 League Tanking Policy</h3>
-    <p>No team may intentionally leave starting roster spots empty. Any evidence of intentionally leaving roster spots empty in order to improve your draft position may be subject to punishment, including but not limited to the forfeiture of draft selections.</p>
+    <h2 class="sectionHeading" bind:this={five}>Section 5 Sidebets</h2>
+    <h3 bind:this={fiveOne}>5.1 Sidebet Definition</h3>
+    <p>A sidebet is any bet between two or more managers that has no impact on how the season plays out, occurring to the 'side' of the real game.</p>
+    <p>Many sidebets shall be purposefully fulfilled during Dude's Weekend</p>
 
-    <h2 class="sectionHeading" bind:this={six}>Section 6 Replacing Managers</h2>
-    
-    <h3 bind:this={sixOne}>6.1 Removing Managers</h3>
-    <p>Short of continuously failing to set a valid lineup, or confirmed collusion, no manager can be removed against their will. Participation, through the league chat, trade offers, and the waiver wire, is strongly encouraged but generally grounds for removal.</p>
-    
-    <h3 bind:this={sixTwo}>6.2 Replacing Managers</h3>
-    <p>When a manager needs to be replaced, the commisiooners will try to find a suitable candidate with some connection to at least a portion of the existing managers. Priority will be placed on managers who are eager to take on a dynasty commitment and will be very active within the league.</p>
-    
-    <h3 bind:this={sixThree}>6.3 Replacement Incentive</h3>
-    <p>In the event that a manager chooses to quit the league or is removed by a comissioner, an incoming manager is offered a 50% reductioon on their first year buy-in.</p>
+    <h3 bind:this={fiveTwo}>5.2 Sidebet Penalty</h3>
+    <p>In the event that a manager does not fulfill their sidebet, they will <strong>DOUBLE</strong> the total dues of the next season. Current total is ${dues * 12}, meaning an 
+    unfulfilled sidebet will cost the manager ${dues * 13}, making the total pot worth ${dues * 24}.</p>
 
-    <h2 class="sectionHeading" bind:this={seven}>Section 7 League Finances</h2>
-    
-    <h3 bind:this={sevenOne}>7.1 League Dues</h3>
-    <p>League dues are set at {dues}$. Dues are collected through <a href="https://www.leaguesafe.com/league/3949641">LeagueSafe</a> and collection will be sent out no later than the first of August. All managers must be fully paid up before the start of regular season. In the event that a manager fails to pay before the deadline, a 10% penalty will be addd to their payment and will go towards the payouts at the end of the year.</p>
-    
-    <h3 bind:this={sevenTwo}>7.2 Payout</h3>
-    <p>League payout is structured as follows:</p>
+    <h3 bind:this={fiveThree}>Historical Unfulfilled Sidebets</h3>
     <ul>
-        <li>1st place: {dues * 8}$</li>
-        <li>2nd place: {dues * 3}$</li>
-        <li>3rd place: {dues}$</li>
+        <li>Matt Dallman, 2020 - Refused to hold up his end of the bargain for the Spam bet vs Victor Markus. Pot doubled in 2021.</li>
+    </ul>
+
+    <h2 class="sectionHeading" bind:this={six}>Section 6 League Finances</h2>
+    
+    <h3 bind:this={sixOne}>6.1 League Dues</h3>
+    <p>League dues are set at {dues}$. Dues are collected through Venmo and collection will be sent out no later than the first of August. All managers must be fully paid up before the draft. In the event that a manager fails to pay before the deadline, they will autodraft. Every week that goes by without payment will be a 10 point deduction in their weekly score.</p>
+    
+    <h3 bind:this={sixTwo}>6.2 Payout</h3>
+    <p>League payout is structured as follows, based on percentages:</p>
+    <p><strong>Addendum: </strong>If a manager violates a sidebet and incurs a penalty, the actual amount will be x2 for all categories.</p>
+    <ul>
+        <li>1st place: ${dues * 5.95}</li>
+        <li>2nd place: ${dues * 2.5}</li>
+        <li>3rd place: ${dues}</li>
+        <li>Weekly High Score: ${dues / (20/3)}</li>
     </ul>
     <p>In the event that a manager was fined due to late payment, their extra fee will be added to the winner's payout. Similarly, if new managers come into the league and the payout is smaller, the difference will come out of the winner's prize.</p>
     
-    <h3 bind:this={sevenThree}>7.3 Raising Dues</h3>
+    <h3 bind:this={sixThree}>6.3 Raising Dues</h3>
     <p>A ⅔ majority vote is required in order to raise league dues. In the event that a manager is no longer financially comfortable with the buyin, finding a co-manager to split the cost is reccomended.</p>
 
 </div>
