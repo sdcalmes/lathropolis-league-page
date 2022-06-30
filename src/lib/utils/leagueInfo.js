79172a5c60a9e62694b65fc960c31c3e76104c6d
sdcalmes <scalmes@wisc.edu>
2022-06-29 23:37:54 -0500

    test publish again

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -256,7 +256,7 @@ export const managers = [
       "favoriteTeam": "gb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Most Transactions", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
-        name: "Matthew Dallman", // Can be anything (usually your rival's name)
+        name: "Dallman", // Can be anything (usually your rival's name)
         link: 0, // manager array number within this array, or null to link back to all managers page
         image: "/managers/question.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
