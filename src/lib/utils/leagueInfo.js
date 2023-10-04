385ebda235bed872d7f2c10bfa6fd49989f706ad
sdcalmes <scalmes@wisc.edu>
2023-10-03 20:19:11 -0500

    Update sleeper id

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "850787395090624512"; // your league ID
+export const leagueID = "992120797269438464"; // your league ID
 export const leagueName = "Lathropolis Fantasy Football"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
